d8480e2e733e1819512e421da98facb7cf00a547
kingdom-0 <kingdom_0@live.cn>
2023-05-12 17:46:26 +0800

    Add nacos config

--- user-service/src/main/java/com/jx/User/UserApplication.java
+++ user-service/src/main/java/com/jx/User/UserApplication.java
@@ -3,12 +3,25 @@ package com.jx.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class UserApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(UserApplication.class, args);
+        while (true){
+            String username = run.getEnvironment().getProperty("user.name");
+            String age = run.getEnvironment().getProperty("user.age");
+            System.out.println("user name:"+username+", age:"+age);
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- user-service/src/main/java/com/jx/User/web/UserController.java
+++ user-service/src/main/java/com/jx/User/web/UserController.java
@@ -4,10 +4,14 @@ import com.jx.User.entity.User;
 import com.jx.User.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author Edison Wang
@@ -24,8 +28,16 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    //@Value("${pattern.dateformat}")
+    private String dateformat;
+
     @GetMapping("/{userId}")
     public User queryById(@PathVariable("userId") long userId){
         return userService.getUserById(userId);
+    }
+
+    @GetMapping("/now")
+    public String getDateTimeNow(){
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformat));
     }
 }
